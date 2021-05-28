a8e3adcc191a563fda048e46f16ffacb7dbe6d74
Rachid Ben said <rachidbensaid01@gmail.com>
2021-05-28 15:26:52 +0200

    Sort leaderboard

--- Assets/Scripts/Helper Scripts/Api.cs
+++ Assets/Scripts/Helper Scripts/Api.cs
@@ -6,7 +6,7 @@ public static class Api
     public static string URL = "https://snake-api-xa8h7.ondigitalocean.app";
     public static LeaderboardModel[] GetLeaderboards()
     {
-        HttpWebRequest request = (HttpWebRequest) WebRequest.Create(URL + "/leaderboards");
+        HttpWebRequest request = (HttpWebRequest) WebRequest.Create(URL + "/leaderboards?_sort=score:DESC");
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
         string json = reader.ReadToEnd();
